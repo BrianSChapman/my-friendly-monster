ec760b74cabefce43199c919a0f98835b635ee3a
Brian Chapman <chapman.brian86@gmail.com>
2023-01-31 19:03:15 -0500

    Fleshed out resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -8,7 +8,12 @@ const resolvers = {
     users: async (parent, { userId }) => {
       return User.findOne({ _id: userId, username, password });
     },
+    monsters: async (parent, { username }) => {
+        const params = username ? { username } : {};
+        return Monster.find(params).sort({ createdAt: -1})
+    }
   },
+
   Mutation: {
     // Add a user and assign them a JWT
     addUser: async (parent, { username, password }) => {
@@ -17,16 +22,16 @@ const resolvers = {
       return { token, user };
     },
   },
-//   Login a user
+  //   Login a user
   login: async (parent, { username, password }) => {
     const user = await user.findOne({ username });
-// checking if the username is valid & throwing an error if not
+    // checking if the username is valid & throwing an error if not
     if (!user) {
       throw new AuthenticationError(
         "Sorry friend! Unable to log you in. Check your username & password. Thanks!"
       );
     }
-// Checking for proper password
+    // Checking for proper password
     const correctPw = await user.isCorrectPassword(password);
 
     if (!correctPw) {
@@ -39,7 +44,37 @@ const resolvers = {
 
     return { token, user };
   },
-//   ADD A USER, UPDATE A MONSTER, ADD A MONSTER, REMOVE A MONSTER.
+
+  addUser: async (parent, { username, password }) => {
+    const user = await User.create({ username, password });
+    const token = signToken(user);
+    return { token, user };
+  },
+
+  addMonster: async (parent, { fullName, createdAt, userId }) => {
+    return Monster.create({ fullName, createdAt, userId });
+  },
+
+  updateMonster: async (parent, { id, userId }) => {
+    return await Monster.findOneAndUpdate(
+        { _id: id },
+        { userId },
+        { new: true }
+    );
+  },
+//  Might need a second opinion on this one in particular)
+  deleteMonster: async (parent, { monsterId }, context) => {
+    if (context.user) {
+        const monster = await Monster.findOneAndDelete({
+         _id: monsterId,
+        });
+        await User.findOneAndUpdate(
+            { _id: context.user._id }
+            { $pull: { monster: monster._id } }
+        );
+        return monster;
+  } 
+}
 };
 
 module.exports = resolvers;
