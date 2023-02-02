35314133c5dc214d50368ff1627227edf2703377
layne nigro <laynenigro@gmail.com>
2023-02-02 11:58:01 -0500

    signup and login logic working

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+export default function Header() {
+
+  return (
+    <>
+<div className="navbar">
+<h1 className="title">My Friendly Monsters</h1>
+</div>
+    </>
+  );
+}
+
+
+
+
--- client/src/pages/HomeContainer.js
+++ client/src/pages/HomeContainer.js
@@ -7,9 +7,6 @@ export default function HomeContainer() {
   return (
     <>
     <section>
-      <div className="navbar">
-        <h1 className="title">My Friendly Monsters</h1>
-      </div>
       <Login/>
     </section>
     </>
