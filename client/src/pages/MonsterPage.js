2f86e185c6a194081583b127f3b12fedc514505c
ColinDeery <colinbdeery@gmail.com>
2023-02-03 14:53:10 -0500

    added placeholder monster image and feed button

--- client/src/pages/MonsterPage.js
+++ client/src/pages/MonsterPage.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { UPDATE_MONSTER, REMOVE_MONSTER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
+import ben from "./assets/ben.gif";
 
 export default function MonsterPage() {
     return (
-        <h1>MONSTER PAGE</h1>
+        <section className="container-fluid">
+            <img src={ben} alt="Ben Gif" />
+            <button className="feedButton">FEED MONSTER</button>
+        </section>
     )
 }
