b65ee11a6f55d6dae014adb36793ce9a38e61076
Brian Chapman <chapman.brian86@gmail.com>
2023-02-02 21:58:47 -0500

    All of the gifs...and some debugging in the EggPage

--- client/src/pages/EggPage.js
+++ client/src/pages/EggPage.js
@@ -2,30 +2,42 @@ import React, { useState } from "react";
 import { ADD_MONSTER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
 import egg from "./assets/zh3j4qF.gif";
+import beety from './assets/beety.gif';
+import ben from './assets/ben.gif';
+import frower from './assets/frower.gif';
+import iceabella from './assets/iceabella.gif';
+import volma from './assets/volma.gif';
+import wavy from './assets/wavy.gif';
+import Auth from '../utils/auth';
 
 const EggPage = () => {
+    const tokenTest = Auth.getProfile();
+  const userId = tokenTest.data._id;
   const monsterArray = [
-    "https://imgur.com/a/6ncbacJ",
-    "https://imgur.com/a/HF9GwLp",
-    "https://imgur.com/a/X3rUCn6",
-    "https://imgur.com/a/L9PCw85",
-    "https://imgur.com/a/u45n7Ln",
-    "https://imgur.com/a/eYXEvhs",
-  ];
+    'wavy',
+    'volma',
+    'iceabella',
+    'frower',
+    'ben',
+    'beety',
+];
   const randomMonster =
     monsterArray[Math.floor(monsterArray.length * Math.random())];
   const [nameState, setNameState] = useState("My Monster");
   console.log(nameState);
   const [imgState, setImgState] = useState(randomMonster);
-  console.log(imgState);
+    console.log(imgState);
   const [addMonster, { error }] = useMutation(ADD_MONSTER);
+
 
   const AddMonster = () => {
     try {
         addMonster({
         variables: {
-          nameState,
-          imgState,
+          fullName: nameState,
+          imageUrl: imgState,
+          userId
+          
         },
       });
     } catch (error) {
@@ -35,7 +47,7 @@ const EggPage = () => {
   return (
     <section className="container-fluid">
       <button onClick={AddMonster}>
-        <img src={egg} alt="Egg Pixel Art"></img>
+        <img src={egg} alt="Egg Pixel Art"/>
       </button>
     </section>
   );
