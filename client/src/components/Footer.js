a9f955322e08b6254a4ff9c1e459516b6fea0409
Brian Chapman <chapman.brian86@gmail.com>
2023-02-03 21:46:29 -0500

    Added more to the footer. Working out logout logic

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,5 +3,7 @@ import React from "react";
 export default function Footer() {
     return (
       <footer className="container-fluid text-center">
+        <p id='footer-text'>Made with &#128147; & 	
+&#127829; by Layne, Anthony, Colin, & Brian</p>
       </footer>
 )}
