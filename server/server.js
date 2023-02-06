9c6ccca58fe018219d14a7913043dc76a6cb56e1
layne nigro <laynenigro@gmail.com>
2023-02-05 23:00:06 -0500

    didnt actually fix anything

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE.ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
