34447313f7f0ac120a002e408be55b35a8d9e565
Brian Chapman <chapman.brian86@gmail.com>
2023-02-06 16:54:34 -0500

    Put AuthMiddleware because we're smart

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,11 +1,30 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // const secret = process.env.SECRET;
-const secret = 'TAMAGOTCHI';
-const expiration = '6h';
+const secret = "TAMAGOTCHI";
+const expiration = "6h";
 
 module.exports = {
+  authMiddleware: function ({ req }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log("Invalid token");
+    }
+  },
+
   signToken: function ({ username, password, _id }) {
     const payload = { username, password, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
