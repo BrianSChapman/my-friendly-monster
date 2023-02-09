912afaeb3c7d0a3c82c4f8ae4ef0f0ea36fc02b1
Anthony Cedrone <antcedrone95@gmail.com>
2023-02-09 12:59:41 -0500

    working on if auth.loggedin

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,18 +4,8 @@ import Auth from "../utils/auth";
 
 export default function Header() {
   // const hideLogout = window.location.pathname === "/" || "/signup";
-
+  if (Auth.loggedIn()) {
   return (
-  (window.location.pathname === "/" || "/signup") ? (
-    <nav className="navbar navbar-expand-lg shadow-lg">
-      <div className="container-fluid">
-        <h1 className="title text-center">My Friendly Monster</h1>
-        <ul className="nav justify-content-end">
-          <li className="nav-item"></li>
-        </ul>
-      </div>
-    </nav>
-  ) : (
     <nav className="navbar navbar-expand-lg shadow-lg">
       <div className="container-fluid">
         <h1 className="title text-center">My Friendly Monster</h1>
@@ -32,5 +22,17 @@ export default function Header() {
         </ul>
       </div>
     </nav>
-  ));
+  );
+  } else {
+    return (
+      <nav className="navbar navbar-expand-lg shadow-lg">
+      <div className="container-fluid">
+        <h1 className="title text-center">My Friendly Monster</h1>
+        <ul className="nav justify-content-end">
+          <li className="nav-item"></li>
+        </ul>
+      </div>
+    </nav>
+    )
+  }
 }
